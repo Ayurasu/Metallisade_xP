366965a27595c20dbf7914c6e2097d890b62fdd1
Ayurasu <sn.kujala@gmail.com>
2023-05-30 14:10:16 +0300

    Assets spawns and disappears after colliding with the ground

--- Metallisade/Assets/Scripts/SpawnManager.cs
+++ Metallisade/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] flyingPrefabs;
+    private float spawnRangeX = 1.5f;
+    private float spawnPosZ = -1;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAsset", startDelay, spawnInterval);
     }
 
-    // Update is called once per frame
+    // Update is called once per frameasse
     void Update()
     {
         
     }
+
+    void SpawnRandomAsset()
+    {
+        int assetIndex = Random.Range(0, flyingPrefabs.Length);
+        //Randomize the location
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 4, spawnPosZ);
+
+        //Spawns the assets
+
+        Instantiate(flyingPrefabs[assetIndex], spawnPos, flyingPrefabs[assetIndex].transform.rotation);
+    }
+   
+
 }
