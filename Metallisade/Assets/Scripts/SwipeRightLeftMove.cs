e6a60cd75e271aebd0c9210ec5c6417a1b156a86
Ayurasu <sn.kujala@gmail.com>
2023-06-13 14:58:33 +0300

    Touch controls work(at least on simulator)

---
+++ Metallisade/Assets/Scripts/SwipeRightLeftMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeRightLeftMove : MonoBehaviour
+{
+
+    private float speed = 0.01f;
+    private Touch touch;
+    public float xRange = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (transform.position.x < -xRange)
+            {
+                transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            }
+            else if (transform.position.x > xRange)
+            {
+                transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            }
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * speed, transform.position.y, transform.position.z);
+            }
+        }
+    }
+}
